701d40b7784c53102bd18d82043dd1da12fca574
lucasmpimentel <lucas.mirandapimentel@gmail.com>
2021-11-14 15:06:29 -0300

    iniciando o projeto: feito dos requisitos de 1 ao 6, correçoes do lint feitas

--- src/challenges.js
+++ src/challenges.js
@@ -1,31 +1,55 @@
 // Desafio 1
-function compareTrue() {
-  // seu código aqui
+function compareTrue(person1, person2) {
+  let result = false;
+  if (person1 && person2 === true) {
+    result = true;
+  }
+
+  return result;
 }
 
 // Desafio 2
-function calcArea() {
-  // seu código aqui
+function calcArea(base, height) {
+  let result = (base * height) / 2;
+  return result;
 }
 
 // Desafio 3
-function splitSentence() {
-  // seu código aqui
+/* Consegui resolver pesquisando aqui: https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Global_Objects/String/split */
+function splitSentence(phrase) {
+  const result = phrase.split(' ');
+  return result;
 }
 
 // Desafio 4
-function concatName() {
-  // seu código aqui
+/* Acessando o link (https://pt.stackoverflow.com/questions/87416/como-retornar-o-%C3%BAltimo-registro-de-um-array-com-javascript-ou-jquery) descobri como capturar o ultimo elemento do array atravez da resposta do: "Jéf Bueno" */
+function concatName(list) {
+  let result = `${list[list.length - 1]}, ${list[0]}`;
+  return result;
 }
 
 // Desafio 5
-function footballPoints() {
-  // seu código aqui
+function footballPoints(wins, ties) {
+  const result = (wins * 3) + ties;
+  return result;
 }
 
 // Desafio 6
-function highestCount() {
-  // seu código aqui
+/* Consegui resolver esse com base nas documentações de Math.max e .filter encontradas respectivamente nos links:
+(1 - https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Global_Objects/Math/max)
+(2 - https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Global_Objects/Array/filter) */
+function highestCount(numbers) {
+  let n = [];
+  let count = [];
+  // Aprendi na documentação como extrair o maior número.
+  n = Math.max(...numbers);
+  // Descobri que pra fazer o filter funcionar deveria utilizar uma função (?)
+  function comp(value) {
+    return value === n;
+  }
+  count = numbers.filter(comp);
+  let result = count.length;
+  return result;
 }
 
 // Desafio 7
